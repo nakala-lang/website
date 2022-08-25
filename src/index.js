6d4bfbe46f62462026b4db10a41a439cc981bed2
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-08-25 15:09:11 -0400

    delete: remove old webpack files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import { App } from "./App";
-
-import "./styles.css"
-
-const app = document.getElementById("root");
-ReactDOM.render(<App />, app);
