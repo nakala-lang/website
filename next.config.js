8a9311534ab962df198903d058f39a2db6d6701e
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2022-08-28 12:27:52 -0400

    add: deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
     config.experiments = {asyncWebAssembly: true };
     return config;
-  }
+  },
+  assetPrefix: isProd ? '/website/' : ''
 }
 
 module.exports = nextConfig
